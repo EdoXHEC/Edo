e630a25bf250a865d4acf2f4f2a687341e0055bb
Deborah Aisenberg <deborahaisenberg@MacBook-Air-de-Deborah.local>
2018-10-23 17:36:06 +0200

    long lat

--- front/src/providers/show-service-rest.ts
+++ front/src/providers/show-service-rest.ts
@@ -77,7 +77,7 @@ export class ShowService {
             {
                 "name": "Soirée : Café Oz (Chatêlet)",
                 "description": "Le Café Oz est un pub anglophone qui véhicule l'image de l'Australie. La convivialité, la simplicité, l'esprit d'équipe et la garantie d'une ambiance décontractée sont autant de valeurs que nous jugeons essentielles au sein de nos établissements. À la recherche d'une clientèle jeune, festive, et sportive, nous avons à coeur de recréer l'énergie et la bonne humeur de la fête à l'anglo-saxonne. Nous mettons également un point d'honneur à diffuser de nombreux événements sportifs sur écrans géants comme le rugby, le football australien ou le cricket pour faire vivre à nos clients la passion du sport façon « Aussie » ! ",
-                "lat": 48.8534099,
+                "lat": 48.8434099,
                 "long": 2.3488001,
                 "address": "18 rue St Denis, 75001 Paris ",
                 "image": "assets/img/cafe.jpg",
@@ -85,7 +85,7 @@ export class ShowService {
             {
                 "name": "Tennis : Partie au TCP",
                 "description": " Location d'un terrain de tennis pour 12€/l'heure dans l'un des plus grand club de tennis de Paris.",
-                "lat": 48.8534099,
+                "lat": 48.8334099,
                 "long": 2.3488001,
                 "address": "5 Avenue Félix d'Herelle, 75016 Paris",
                 "image": "assets/img/tennis.jpg",
@@ -93,7 +93,7 @@ export class ShowService {
             {
                 "name": "Fête des Vendanges de Montmartre",
                 "description": "La Fête des vendanges de Montmartre célèbre annuellement, depuis 1934, l'arrivée des cuvées issues du Clos Montmartre. L'organisation est assurée par la mairie du 18e arrondissement, mobilise les acteurs de la vie locale (commerçants, artistes, associations, écoles, ...) et invite des célébrités à parrainer l’événement. ",
-                "lat": 48.8534099,
+                "lat": 48.8734099,
                 "long": 2.3488001,
                 "address": "Montmartre, 75019 Paris",
                 "image": "http://s1.lprs1.fr/images/2017/10/10/7321206_1c09fe12-ad0b-11e7-8c52-9592d7f48354-1_1000x625.jpg"
